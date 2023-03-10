95e484360e8fb40987fe418f58a8f50222f16696
ykoksovos <yasar.kok@sovos.com>
2023-03-11 00:52:40 +0300

    spring data jpa added to build.gradle and Account entity is modified

--- src/main/java/com/eteration/simplebanking/model/Account.java
+++ src/main/java/com/eteration/simplebanking/model/Account.java
@@ -3,5 +3,21 @@ package com.eteration.simplebanking.model;
 
 // This class is a place holder you can change the complete implementation
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+@Data
 public class Account {
+
+    @Id
+    private String accountNumber;
+    private String owner;
+    private double balance;
+    private LocalDate createDate;
+    private Long transactionID;
+
 }
